2a98d4f231c98d165dd835ce85fb1d93d7136adb
jdvpl <juanda554242@gmail.com>
2020-09-19 00:24:23 -0500

    Primeros pasos en python

---
+++ primeros pasos/holamundo.py
@@ -0,0 +1,10 @@
+# imprimiendo algo en consola
+
+print("insecto")
+
+""""
+asi se hacen los comentarios donde implica mas de una linea
+ejjeje
+
+"""
+print("Adios")
---
+++ primeros pasos/variable.py
@@ -0,0 +1,26 @@
+# """ variables es un contenedor de informacion que 
+#     luego puede ser aprovechada
+# """
+
+# creando variables
+texto="Master en Python"
+texto2="Voy a ser un full stack developer"
+num=3
+num2=5
+print(texto)
+print(texto2)
+print(num)
+print(num2)
+print("##########################".center(50))
+print("Cambia las variables".center(50))
+num=25
+num2=36
+print(num)
+print(num2)
+
+# concatenacion
+nombre="Juan Daniel Suarez"
+Profesion="Full Stack Developer"
+Web="Jdvpl.com"
+
+print("Nombre: "+nombre+ " es un "+ Profesion+ " y su Sitio web es: "+Web)
