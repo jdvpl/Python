3887fac84ebe02b1d96b8cdb9ede0a22d0428b85
jdvpl <juanda554242@gmail.com>
2020-09-19 23:21:23 -0500

    terminado el time.sleep

--- primeros pasos/time.py
+++ primeros pasos/time.py
@@ -1,6 +1,6 @@
 #para que se demore el codigo
 import time
-jiren="Feliz dia perritos"
-for character in jiren:
-    print(character, end="")
+x=("Feliz dia del amor y la amistad a todos, les desea JDVPL Juan Daniel")
+for c in x:
+    print(c, end="")
     time.sleep(0.1)
