b6b5e685cf27fdf52ccb7a87960a1ad91fa5aa75
jdvpl <juanda554242@gmail.com>
2020-09-19 23:57:55 -0500

    tipos de datos

---
+++ tipodedatos.py
@@ -0,0 +1,47 @@
+#tipo de dato nada
+nada=None #no tiene nada la variable
+print(nada)
+#mostrando tipo de dato
+print(type(nada))
+#tipo de dato String o cadena
+Nombre="Hola soy un maldtio insecto1"
+print(Nombre)
+print(type(Nombre))
+#tipo de dato entero
+entero=99
+print(entero)
+print(type(entero))
+#tipo de dato flotante
+flotante=3.1425
+print(flotante)
+print(type(flotante))
+#tipo de dato boleano
+boleano=True
+print(boleano)
+print(type(boleano))
+#tipo de dato lista
+lista=[10,20,35.3,36,9,"Jiren",25,"kamisama"] # se pueden varios tipos de datos
+print(lista)
+print(type(lista))
+#tipo de dato tupa (datos que no cambian son fijos)
+tupanocambia=("Fulls estack","Developer",26)
+print(tupanocambia)
+print(type(tupanocambia))
+#tipo de dato diccionario, se parecen a lso archivos json tipo clave y valor
+diccionario={
+    "Nombre":"Juan Daniel",
+    "Apellido":"Suarez",
+    "Profesion":"Desarrollador",
+    "Cursos":["Java","React","Angular","Javascript","Php"]#se pueden colocar varios tipos de datos dentro del diccionario
+
+}
+print(diccionario)
+print(type(diccionario))
+#tipo de dato rango
+rango=range(9)
+print(rango)
+print(type(rango))
+#typo de dato byte
+dato_byte=b"probando"
+print(dato_byte)
+print(type(dato_byte))
