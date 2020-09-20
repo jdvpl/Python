1227a985a83fd293815c72525b2cf606513556b5
jdvpl <juanda554242@gmail.com>
2020-09-20 18:57:01 -0500

    elif

---
+++ 4 condicionales/elif.py
@@ -0,0 +1,18 @@
+print(f"########################## elif ####################")
+dia=int(input("Introduce el dia de la semana "))
+if dia==1:
+    print(f"es lunes")
+elif dia==2:
+    print(f"es martes")
+elif dia==3:
+    print(f"es miercoles")
+elif dia==4:
+    print(f"es jueves")
+elif dia==5:
+    print(f"es viernes")
+elif dia==6:
+    print(f"es jueves")
+elif dia==7:
+    print(f"es domingo")
+else:
+    print(f"opcion incorrecta")
