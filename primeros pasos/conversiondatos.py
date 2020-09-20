65fa58f222614a00099e724c21378f804c7ee989
jdvpl <juanda554242@gmail.com>
2020-09-20 00:32:35 -0500

    curiosidades de loss strings o cadenas, salto de linea, tabulacion, comillas, borrado anterior \r

--- primeros pasos/conversiondatos.py
+++ primeros pasos/conversiondatos.py
@@ -13,3 +13,15 @@ print(jiren)
 print(type(jiren))
 print(kaka)
 print(type(kaka))
+
+#strings variables structura mostrando comillas en el print
+#para colocar comillas dentro de un string seria 1 comenzando con comillas doble y el texto en comillas simples o viceversa
+#la otra es con las barras inclinadas
+mi_texto="jiren"
+texto_2="es una kisama"
+texto_comillas="es del \"universo 11\" " #se coloca barra inclinada 
+texto_unido=(f"{mi_texto} {texto_2}")#se puede asi o con el mas es mas bonita esta forma
+print(texto_unido)
+print(f"{mi_texto} \n{texto_2}") #salto de linea
+print(f"{mi_texto} \t{texto_2}") #espacio grande tabulacion
+print(f"{mi_texto} \r{texto_2}") #borra todo lo anterior del \r
