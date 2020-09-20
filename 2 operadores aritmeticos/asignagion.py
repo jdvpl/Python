f6642031a2e95ed96d5c2944c05f5671d6f27bbb
jdvpl <juanda554242@gmail.com>
2020-09-20 17:10:33 -0500

    operadores de asignacion

---
+++ 2 operadores aritmeticos/asignagion.py
@@ -0,0 +1,22 @@
+edad=55
+print(f"{edad}")
+
+#es sumandoles
+
+edad+=5
+print(edad)
+edad-=10
+print(edad)
+#operadores de incremento y decremento asi como otro lenguaje year++ o year--
+
+year=2021
+year=year+1
+year+=1
+year-=1
+
+print(year)
+#operadores de predecremento y preincremento
+
+year=1-year
+year=1+year
+print(year)
