aaef694841e698f822ad24484e4a953c896fc633
jdvpl <juanda554242@gmail.com>
2020-09-21 00:06:59 -0500

    pasandoles parametros a las funciones

--- 7 funciones/funcion1.py
+++ 7 funciones/funcion1.py
@@ -1,12 +1,34 @@
-"""
-una funcion es un conjutno de instrucciones bajo un nombre concreto que puede 
-reutilizarse incovando a la funcion tantas veces como sea necesario
-def nombredelafuncon(parametros):
-    #bloque de codigo o conjunto de funcones
-nombredelafuncion(mi_parametro)
-"""
-#ejemrplo 1
-print("mi primera funcion ")
-def muestraNombre():
-    print("jdvpl")
-muestraNombre()
+# """
+# una funcion es un conjutno de instrucciones bajo un nombre concreto que puede 
+# reutilizarse incovando a la funcion tantas veces como sea necesario
+# def nombredelafuncon(parametros):
+#     #bloque de codigo o conjunto de funcones
+# nombredelafuncion(mi_parametro)
+# """
+# #ejemrplo 1
+# print("mi primera funcion ")
+# def muestraNombre():
+#     print("jdvpl")
+# muestraNombre()
+
+# pasando parametros a las funciones
+print("ejemplo #2")
+Nombre=input("escribe tu nombre")
+edad=int(input("ingresa la edad "))
+def mostrarTunombre(nombre,edad):
+    if edad>=18:
+        print(f"Tu nombre es: {nombre}, y eres mayor de edad")
+    else:
+        print(f"Tu nombre es: {nombre}, y eres un guipa jajaja")
+mostrarTunombre(Nombre,edad)
+
+
+#con bucles
+# canti=int(input("cuantos nombres quieres "))
+# conta=1
+# while conta<=canti:
+#     nombre1=input(f"Digite el nombre {conta} ")
+#     def mostrarNombre(final):
+#         print(f"Tu nombre es: {final}")
+#     mostrarNombre(nombre1)
+#     conta+=1
