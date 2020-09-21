dcda48786cc52605886065823c85e1550c47c17a
jdvpl <juanda554242@gmail.com>
2020-09-20 20:03:54 -0500

    bucle while

---
+++ 5 bucles/while.py
@@ -0,0 +1,28 @@
+# el el while se controla el contador y con una condicion
+"""
+es una estructura de control que repite la ejecucion de una serie de instrucciones
+tantas veces como sea necesario hasta que se deje de cumplir la condicion
+es importante que haya un contadores
+contador=0
+
+while condicion:
+    bloque de instrucciones
+    actualizacion de contador
+
+"""
+contador=1
+while contador<=100:
+    print(f"Este es el numero: {contador}")
+    contador+=1
+
+
+#con comas
+print("\n\n\n\n")
+contador=1
+muestrame=str(0)
+while contador<=100:
+    muestrame=muestrame+", "+str(contador)
+    contador+=1
+
+print(muestrame)
+
