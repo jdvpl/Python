6f1a0aa6d00e6744dbe5cb1de64b1a9ddf5306cf
jdvpl <juanda554242@gmail.com>
2020-09-21 11:53:43 -0500

    reto 7 u nacional calculo de areas con funciones

---
+++ 6 ejercicios/retio7funcionesarea.py
@@ -0,0 +1,19 @@
+import math
+pi=math.pi
+def rectangulo(a,b):
+    return a*b
+def circulo(r):
+    return pi*r**2
+
+sumaTotal=0
+a1=float(input("ingrese el valor uno "))
+b1=float(input("ingrese el valor dos "))
+sumaTotal+=rectangulo(a1,b1)
+a2=float(input("ingrese el valor uno "))
+b2=float(input("ingrese el valor dos "))
+sumaTotal+=rectangulo(a2,b2)
+r1=float(input("ingrese el valor uno "))
+sumaTotal+=circulo(r1)
+r2=float(input("ingrese el valor dos "))
+sumaTotal+=circulo(r2)
+print(f"suma total= {sumaTotal}")
