9c976d0276138a5cb288c9ea732e73cbf5cd8399
jdvpl <juanda554242@gmail.com>
2020-09-20 23:14:59 -0500

    porcentajes

---
+++ 6 ejercicios/ejercicio6porcentaje.py
@@ -0,0 +1,8 @@
+"""
+porcentaje de numero
+"""
+num=int(input("Ingrese el numero "))
+porce=float(input(f"Que porcentaje quieres sacarle al numero {num} "))
+
+resul=(num*(porce/100))
+print(f"El {porce}% de {num} es: {resul}")
