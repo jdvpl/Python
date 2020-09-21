ee72fe5433bb058a3d643da3605943ebf61c5115
jdvpl <juanda554242@gmail.com>
2020-09-20 23:49:03 -0500

    funciones sencillas

---
+++ 7 funciones/funcion1.py
@@ -0,0 +1,12 @@
+"""
+una funcion es un conjutno de instrucciones bajo un nombre concreto que puede 
+reutilizarse incovando a la funcion tantas veces como sea necesario
+def nombredelafuncon(parametros):
+    #bloque de codigo o conjunto de funcones
+nombredelafuncion(mi_parametro)
+"""
+#ejemrplo 1
+print("mi primera funcion ")
+def muestraNombre():
+    print("jdvpl")
+muestraNombre()
