7fceaf7e586c12bd1ec4f4aea504ae5302d250bf
jdvpl <juanda554242@gmail.com>
2020-09-20 19:02:40 -0500

    operadores logicos

--- 4 condicionales/elif.py
+++ 4 condicionales/elif.py
@@ -1,18 +1,31 @@
+# print(f"########################## elif ####################")
+# dia=int(input("Introduce el dia de la semana "))
+# if dia==1:
+#     print(f"es lunes")
+# elif dia==2:
+#     print(f"es martes")
+# elif dia==3:
+#     print(f"es miercoles")
+# elif dia==4:
+#     print(f"es jueves")
+# elif dia==5:
+#     print(f"es viernes")
+# elif dia==6:
+#     print(f"es jueves")
+# elif dia==7:
+#     print(f"es domingo")
+# else:
+#     print(f"opcion incorrecta")
+#operadores logicos
+#and =y
+#or o
+#! negacion
+#not no
 print(f"########################## elif ####################")
-dia=int(input("Introduce el dia de la semana "))
-if dia==1:
-    print(f"es lunes")
-elif dia==2:
-    print(f"es martes")
-elif dia==3:
-    print(f"es miercoles")
-elif dia==4:
-    print(f"es jueves")
-elif dia==5:
-    print(f"es viernes")
-elif dia==6:
-    print(f"es jueves")
-elif dia==7:
-    print(f"es domingo")
+edad=int(input("Introduce la edad "))
+edad_minima=18
+edad_maxima=65
+if edad>=edad_minima and edad<=edad_maxima:
+    print(f"esta en edad de trabajar")
 else:
-    print(f"opcion incorrecta")
+    print("no esta en edad de trabajar")
