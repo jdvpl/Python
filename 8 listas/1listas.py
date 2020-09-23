e8d9be3648e2f6bc229362ec6d91e7332b152046
Juan Daniel <juanda554242@gmail.com>
2020-09-23 12:22:38 -0500

    imprimiendo datos de las listas indices reemplazando datos de las listas, etc

--- 8 listas/1listas.py
+++ 8 listas/1listas.py
@@ -9,18 +9,29 @@ pelicula="batman"
 # definir una lista
 peliculas=["goku","spiderman","sr de los anillos","broly"]
 # otra definicion de lista
-cantantes=list(("bon jovi","metallica","michael jason"))
+cantantes=list(("bon jovi","metallica","michael jason","romero santos","enrique"))
 # lista con rangos
 anos=list(range(2020,2050))
 # lista variada
 variada=["kakaroto",30,25.6,False,"texo"]
 
 
-print(pelicula)
-# lista de peliculas
+# print(pelicula)
+# # lista de peliculas
+# print(peliculas)
+# # lista de cantantes
+# print(cantantes)
+# # lsuita de anos
+# print(anos)
+# # lista variada
+
+# reasignando valores a un indice de la lista
+peliculas[2]="popeye"
+# indices
 print(peliculas)
-# lista de cantantes
-print(cantantes)
-# lsuita de anos
-print(anos)
-# lista variada
+print(peliculas[1])
+print(peliculas[-1]) #se devuelve en el primer puesto al reves
+print(cantantes[1:4]) #imprime los de la posicion 1 al 4 menos una posicion osea que comienza del 1 hasta 3
+print(cantantes[1:])#todos los que hay apartir de la posicon 1
+print(cantantes[:2]) #todos los anteriores a la posicion 2 osea el 0 y el 1
+
