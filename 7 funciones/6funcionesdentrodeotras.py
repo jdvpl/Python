0cd793036672fc5b54ea09052e9928029a388c9e
Juan Daniel <juanda554242@gmail.com>
2020-09-23 10:37:09 -0500

    funcion lambda

--- 7 funciones/6funcionesdentrodeotras.py
+++ 7 funciones/6funcionesdentrodeotras.py
@@ -1,14 +1,20 @@
-def getNombre(nombre):
-    texto=f"el nombre es: {nombre}"
-    return texto
-def getApellidos(apellidos):
-    texto=f"Los apellidos son: {apellidos}"
-    return texto
+# def getNombre(nombre):
+#     texto=f"el nombre es: {nombre}"
+#     rmbda
+# eturn texto
+# def getApellidos(apellidos):
+#     texto=f"Los apellidos son: {apellidos}"
+#     return texto
 
-print(getNombre("jiren"),getApellidos("suarez"))
+# print(getNombre("jiren"),getApellidos("suarez"))
 
-def devuelveTodo(nombre, apellidos):
-    texto=f"{getNombre(nombre)} \n{getApellidos(apellidos)}"
-    return texto
+# def devuelveTodo(nombre, apellidos):
+#     texto=f"{getNombre(nombre)} \n{getApellidos(apellidos)}"
+#     return texto
 
-print(devuelveTodo("kakaroto","suarez"))
+# print(devuelveTodo("kakaroto","suarez"))
+
+# funcion lambda //funcion anonima que no requiere renombrarla como def 
+dime_el_ano=lambda year:f"el año es: {year*50}"
+
+print(dime_el_ano(2021))
