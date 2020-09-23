e5593b5ae3367a63f90eea303c64b50ec215dedc
Juan Daniel <juanda554242@gmail.com>
2020-09-23 12:00:54 -0500

    listas

---
+++ 8 listas/1listas.py
@@ -0,0 +1,26 @@
+"""\
+
+listas(arrays)
+son colecciones o conjuntos de datos/valores, bajo un unico nombre
+para acceder a esos valores podemos usar un indica numericos
+
+"""
+pelicula="batman"
+# definir una lista
+peliculas=["goku","spiderman","sr de los anillos","broly"]
+# otra definicion de lista
+cantantes=list(("bon jovi","metallica","michael jason"))
+# lista con rangos
+anos=list(range(2020,2050))
+# lista variada
+variada=["kakaroto",30,25.6,False,"texo"]
+
+
+print(pelicula)
+# lista de peliculas
+print(peliculas)
+# lista de cantantes
+print(cantantes)
+# lsuita de anos
+print(anos)
+# lista variada
