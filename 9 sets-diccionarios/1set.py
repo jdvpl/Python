8ae9b58b9d470c092104619d1c76985547494213
Juan Daniel <juanda554242@gmail.com>
2020-09-25 09:42:28 -0500

    sets y funcion add y remove

---
+++ 9 sets-diccionarios/1set.py
@@ -0,0 +1,15 @@
+"""
+set es un tipo de dato para tener una coleecion de valores,
+pero no tiene ni indice ni orden los coloca como se le da la gana jajaj
+
+"""
+personas={
+    "kakaroto",
+    "jiren",
+    "toppo",
+    "vegeta"
+}
+personas.add("Naruto") #para agregar un dato al set
+personas.remove("toppo")
+print(type(personas))
+print(personas)
