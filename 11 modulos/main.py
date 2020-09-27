0b98641cdb53ef89811c8265c1c3d0384e52654a
Juan Daniel <juanda554242@gmail.com>
2020-09-27 18:14:54 -0500

    cambio

---
+++ 11 modulos/main.py
@@ -0,0 +1,8 @@
+"""
+son funcionalidades ya hechas para reutilizar
+
+https://docs.python.org/3/py-modindex.html
+
+podemos conseguir modulos que viene en el lenguaje,
+modulos en internet y podemos crear modulos
+"""
--- Universidad Nacional/practicas/8covid19incrementologaritmos.py
+++ Universidad Nacional/practicas/8covid19incrementologaritmos.py
@@ -1,5 +1,6 @@
 """
-A partir del día 1, el país A tiene una población contagiada de Divoc 9012 de n (1 <=n <= 10^8) y el país B tiene una población contagiada de Divoc 9012 de m (1 <= m <= n). Las tasas de crecimiento diario son de 2% y 3% respectivamente. Desarrollar un algoritmo para informar en que día la población contagiada del país B supera a la del país A.
+A partir del día 1, el país A tiene una población contagiada de Divoc 9012 de n (1 <=n <= 10^8) y el país B tiene una población contagiada de Divoc 9012 de m (1 <= m <= n). Las tasas de crecimiento diario son de 2% y 3% respectivamente.
+ Desarrollar un algoritmo para informar en que día la población contagiada del país B supera a la del país A.
 Entrada
 La entrada consta de dos números reales que representan la cantidad de gente contagiada.
 
