3ca41d5d9e1d23a0db8cf8f8054c7f24a41f54a1
Juan Daniel <juanda554242@gmail.com>
2020-09-28 12:48:07 -0500

    modulo matematicas

---
+++ 11 modulos/math.py
@@ -0,0 +1,15 @@
+import math
+import random
+
+# raiz cuadrada
+
+x=5
+print(f"la raiz cuadra de {5} es: {math.sqrt(x)}")
+print(f"Pi {math.pi}")
+# redondear
+print(f"Reondear con ceil al siguiente numero: {math.ceil(6.3455555)}")
+
+print(f"Reondear con ceil al inicio del numero: {math.floor(6.9955555)}")
+
+# modulo random
+print(f"Numero aleatorio entre 15 y 67 es: {random.randint(15,67)}")
