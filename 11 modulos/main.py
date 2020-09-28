818968462a24c628a9320bb6cfa828628ab2415b
Juan Daniel <juanda554242@gmail.com>
2020-09-28 12:05:39 -0500

    modulo datetime

--- 11 modulos/main.py
+++ 11 modulos/main.py
@@ -15,8 +15,36 @@ from modulo import calculadora #solo para una funciondsa
 #colocarle la palabra modulo..
 from modulo import * 
 
+# modulo fecha
+import datetime
+
 # print(modulo.saludos("Kakaroto"))
 # print(modulo.calculadora(5,6,True))
 
 print(calculadora(5,6,True))
 print(saludos("Jiren"))
+
+# imprimir la fecha de hoy
+print("\n-------------fecha de hoy ------------\n")
+print(datetime.date.today())
+
+fecha_completa=datetime.datetime.now()
+print("\n-------------fecha completa con hora------------\n")
+print(fecha_completa)
+
+print("\n-------------ano de hoy------------\n")
+print(fecha_completa.year)
+print("\n-------------mes de hoy------------\n")
+print(fecha_completa.month)
+print("\n-------------dia de hoy------------\n")
+print(fecha_completa.day)
+print("\n-------------fecha personalidada------------\n")
+
+personalizada=fecha_completa.strftime("%d de %m del %Y, %H:%M:%S")
+print(personalizada)
+
+print("\n-------------fecha timestamp------------\n")
+print(fecha_completa.now().timestamp())
+
+print("\n-------------fecha time------------\n")
+print(fecha_completa.now().times())
