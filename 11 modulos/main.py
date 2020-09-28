2d1d58745c70d5316ab80e879e676d647b171fef
Juan Daniel <juanda554242@gmail.com>
2020-09-28 11:32:23 -0500

    modulos  from nombre_archivo import * importa todas las funciones para reutilizarlas

--- 11 modulos/main.py
+++ 11 modulos/main.py
@@ -6,9 +6,17 @@ https://docs.python.org/3/py-modindex.html
 podemos conseguir modulos que viene en el lenguaje,
 modulos en internet y podemos crear modulos
 """
-import modulo
+#importando todos los modulos pero colocando la palabra modulo el nombre del
+#archivo
+# import modulo
 
-# from modulo import calculadora #solo para una funcion
+from modulo import calculadora #solo para una funciondsa
+#asi se importan todas las funciones que estan en el modulo para no 
+#colocarle la palabra modulo..
+from modulo import * 
 
-print(modulo.saludos("Kakaroto"))
-print(modulo.calculadora(5,6,True))
+# print(modulo.saludos("Kakaroto"))
+# print(modulo.calculadora(5,6,True))
+
+print(calculadora(5,6,True))
+print(saludos("Jiren"))
