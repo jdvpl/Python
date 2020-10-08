8caed37f8046b2ce12669fefe70119326f7e8825
Juan Daniel <juanda554242@gmail.com>
2020-10-08 16:03:16 -0500

    mover archivos

--- 13sistema_archivos/ficheros.py
+++ 13sistema_archivos/ficheros.py
@@ -7,7 +7,6 @@ import pathlib
 
 # abrir archivo
 ruta=str(pathlib.Path().absolute()) + "/13sistema_archivos/texto.txt"
-print(ruta)
 # abre el archibo
 archivo=open(ruta,"a+")
 
@@ -16,12 +15,16 @@ archivo=open(ruta,"a+")
 archivo.write("******************Soy un texto metidos desde python kisamado*********\n")
 # cerrar archivo
 archivo.close()
-
+"""
 #copiar un archivo
 ruta_original=str(pathlib.Path().absolute()) + "/13sistema_archivos/texto.txt"
 ruta_nueva=str(pathlib.Path().absolute()) + "/13sistema_archivos/texto_nuevo_copiados.txt"
 ruta_alternativa=str(pathlib.Path().absolute()) + "/6 ejercicios/texto_nuevo_copiados.txt"
 shutil.copyfile(ruta_original,ruta_alternativa)
 print(ruta_alternativa)
+"""
+# mover un archivo
+ruta_original=str(pathlib.Path().absolute()) + "/13sistema_archivos/texto.txt"
+ruta_nueva=str(pathlib.Path().absolute()) + "/13sistema_archivos/FiCHERO COPIADO.txt "
 
-    
+shutil.move(ruta_original,ruta_nueva)
