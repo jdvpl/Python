35b6c0a80cd95662c87fa47da9e0392f53b7d34f
Juan Daniel <juanda554242@gmail.com>
2020-10-10 12:04:53 -0500

    excepciones personalizadas

---
+++ 14 excepciones/personalizadas.py
@@ -0,0 +1,27 @@
+import os
+import pathlib
+bandera=True
+while bandera:
+    try:
+        nombre=input("Introduce el nombre: ")
+        edad=int(input("edad: "))
+        if edad<5or edad>110:
+            raise ValueError("La edad introducida no es real")
+        elif len(nombre)<1:
+            raise ValueError("Ingrese un nombre correcto: ")
+        else:
+            print("Bienvenido al master en python ",nombre)
+            bandera=False
+    except ValueError:
+        print("Introduce los datos correctamente\n")
+    except Exception as e:
+        print(f"Existe un error")
+
+s=input("Deseas ver el directorio? ")
+if s=="s" or s=="S":
+    direcctorio=os.listdir("./14 excepciones")
+    for i in direcctorio:
+        print(i)
+else:
+    print(f"Gracias por estar aqui! {nombre}")
+
