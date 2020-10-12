591daa829b6d9f8212dcdfd0feb635d8a15927df
Juan Daniel <juanda554242@gmail.com>
2020-10-12 17:44:05 -0500

    imprimendo los datos del objeto

--- 15-clases-POO/main.py
+++ 15-clases-POO/main.py
@@ -12,4 +12,27 @@ class Coche:
     velocidad=300
     caballaje=500
     plazas=2
+    # metodos que hace el objeto
 
+    def acelerar(self):
+        self.velocidad+=1
+    def frenar(self):
+        self.velocidad-=1
+    def getVelocidad(self):
+        return self.velocidad
+
+# fin definicion clase
+# uso de la clase/instanciarla
+
+coche=Coche()
+print(f"""El coche {coche.marca} modelo: {coche.modelo} tiene una velocidad
+de: {coche.velocidad} una aceleracion de {coche.acelerar()}\n""")
+print(f"""La velocidad actual es: {coche.velocidad}""")
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.frenar()
+print(f"""La velocidad nueva es: {coche.velocidad}""")
