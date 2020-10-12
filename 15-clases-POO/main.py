6bf4f48dd4923ffb617a027cf6014ac7b2906aba
Juan Daniel <juanda554242@gmail.com>
2020-10-12 17:56:55 -0500

    los setters y los getters

--- 15-clases-POO/main.py
+++ 15-clases-POO/main.py
@@ -7,13 +7,20 @@ class Coche:
     # atributos o propiedades de la clase
     # caracteristicas del coche
     color="Negro"
-    marca="Ferrario"
+    marca="Ferrari"
     modelo="Aventador"
     velocidad=300
     caballaje=500
     plazas=2
     # metodos que hace el objeto
-
+    def setColor(self,color): #metodo set para colocarle un dato a este auto
+        self.color=color
+    def getColor(self):
+        return self.color
+    def setModelo(self,modelo):
+        self.modelo=modelo
+    def getModelo(self):
+        return self.modelo
     def acelerar(self):
         self.velocidad+=1
     def frenar(self):
@@ -24,10 +31,16 @@ class Coche:
 # fin definicion clase
 # uso de la clase/instanciarla
 
+
+
 coche=Coche()
-print(f"""El coche {coche.marca} modelo: {coche.modelo} tiene una velocidad
-de: {coche.velocidad} una aceleracion de {coche.acelerar()}\n""")
-print(f"""La velocidad actual es: {coche.velocidad}""")
+coche.setColor(input(f"Indique el color del {coche.marca}: "))
+coche.setModelo(input(f"Indique el modelo del {coche.marca}: "))
+
+print(f"""El coche {coche.marca} Color: {coche.getColor()} modelo: {coche.getModelo()} tiene una velocidad
+de: {coche.velocidad} una aceleracion de {coche.getVelocidad()}\n""")
+
+print(f"""La velocidad actual es: {coche.getVelocidad()}""")
 coche.acelerar()
 coche.acelerar()
 coche.acelerar()
@@ -35,4 +48,4 @@ coche.acelerar()
 coche.acelerar()
 coche.acelerar()
 coche.frenar()
-print(f"""La velocidad nueva es: {coche.velocidad}""")
+print(f"""La velocidad nueva es: {coche.getVelocidad()}""")
