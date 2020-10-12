645ef1d78196250779ce61e3a7a39314f70228e0
Juan Daniel <juanda554242@gmail.com>
2020-10-12 17:30:57 -0500

    definiendo un clase POO

---
+++ 15-clases-POO/main.py
@@ -0,0 +1,15 @@
+# programacion orientada a objetos
+
+# definir una clase (monde para crar mas objetos de ese tipo)
+# coche con caracteristicas similares
+
+class Coche:
+    # atributos o propiedades de la clase
+    # caracteristicas del coche
+    color="Negro"
+    marca="Ferrario"
+    modelo="Aventador"
+    velocidad=300
+    caballaje=500
+    plazas=2
+
