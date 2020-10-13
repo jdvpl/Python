ae6fd95b02a98ff3ccd334e1b1f7d74b1b7fbe2f
Juan Daniel <juanda554242@gmail.com>
2020-10-12 23:23:21 -0500

    insertando varios documentos en la base de datos MONGO

--- Universidad Nacional/mongo/mongo.py
+++ Universidad Nacional/mongo/mongo.py
@@ -4,22 +4,33 @@ from pymongo import MongoClient
 
 cliente=MongoClient('localhost')
 # *muetra las bases de datos que hay creadas
-
 db=cliente['prueba']
 col=db['personas']
 # insertar un documento en mongo
-dicccionario={}
 
+def diccionarios(nom,ed,len,hobb):
+    dicccionario={}
+    dicccionario['nombre']=nom
+    dicccionario['edad']=ed
+    dicccionario['lenguajes']=len
+    dicccionario['hobbies']=hobb
+    return dicccionario
+
+# preguntando el primera diccionario
 nombre=input("Ingrese el nombre: ")
 edad=int(input("Ingrese la edad: "))
 lenguajes=input("Ingresa los lenguajes separados por una ',': ").split(",")
-hobbies=input("Ingresa tus hobbies separados por una coma ").split(",")
-dicccionario['nombre']=nombre
-dicccionario['edad']=edad
-dicccionario['lenguajes']=lenguajes
-dicccionario['hobbies']=hobbies
+hobbies=input("Ingresa tus hobbies separados por una ',': ").split(",")
 
-col.insert_one(dicccionario)
+# preguntando el segundo diccionario
+nombre1=input("Ingrese el nombre: ")
+edad1=int(input("Ingrese la edad: "))
+lenguajes1=input("Ingresa los lenguajes separados por una ',': ").split(",")
+hobbies1=input("Ingresa tus hobbies separados por una ',': ").split(",")
+
+#!insertando varios documentos a la base de datos
+# print([diccionarios(nombre,edad,lenguajes,hobbies),diccionarios(nombre1,edad1,lenguajes1,hobbies1)])
+col.insert_many([diccionarios(nombre,edad,lenguajes,hobbies),diccionarios(nombre1,edad1,lenguajes1,hobbies1)])
 
 # para saber cuantos documentos hay en la coleccion
 print(col.count_documents({}))
