e74331164b91d7ee87a974800281534ea6140a42
Juan Daniel <juanda554242@gmail.com>
2020-10-13 12:51:15 -0500

    mongo

--- Universidad Nacional/mongo/mongo.py
+++ Universidad Nacional/mongo/mongo.py
@@ -8,28 +8,28 @@ db=cliente['prueba']
 col=db['personas']
 # insertar un documento en mongo
 
-# def diccionarios(nom,ed,len,hobb):
-#     dicccionario={}
-#     dicccionario['nombre']=nom
-#     dicccionario['edad']=ed
-#     dicccionario['lenguajes']=len
-#     dicccionario['hobbies']=hobb
-#     return dicccionario
+def diccionarios(nom,ed,len,hobb):
+    dicccionario={}
+    dicccionario['nombre']=nom
+    dicccionario['edad']=ed
+    dicccionario['lenguajes']=len
+    dicccionario['hobbies']=hobb
+    return dicccionario
 
-# # preguntando el primera diccionario
-# nombre=input("Ingrese el nombre: ")
-# edad=int(input("Ingrese la edad: "))
-# lenguajes=input("Ingresa los lenguajes separados por una ',': ").split(",")
-# hobbies=input("Ingresa tus hobbies separados por una ',': ").split(",")
+# preguntando el primera diccionario
+nombre=input("Ingrese el nombre: ")
+edad=int(input("Ingrese la edad: "))
+lenguajes=input("Ingresa los lenguajes separados por una ',': ").split(",")
+hobbies=input("Ingresa tus hobbies separados por una ',': ").split(",")
 
-# # preguntando el segundo diccionario
-# nombre1=input("Ingrese el nombre: ")
-# edad1=int(input("Ingrese la edad: "))
-# lenguajes1=input("Ingresa los lenguajes separados por una ',': ").split(",")
-# hobbies1=input("Ingresa tus hobbies separados por una ',': ").split(",")
+# preguntando el segundo diccionario
+nombre1=input("Ingrese el nombre: ")
+edad1=int(input("Ingrese la edad: "))
+lenguajes1=input("Ingresa los lenguajes separados por una ',': ").split(",")
+hobbies1=input("Ingresa tus hobbies separados por una ',': ").split(",")
 
-# #!insertando varios documentos a la base de datos
-# col.insert_many([diccionarios(nombre,edad,lenguajes,hobbies),diccionarios(nombre1,edad1,lenguajes1,hobbies1)])
+#!insertando varios documentos a la base de datos
+col.insert_many([diccionarios(nombre,edad,lenguajes,hobbies),diccionarios(nombre1,edad1,lenguajes1,hobbies1)])
 
 #*imprimiendo los datos que hay en una colleccion
 print("Imprimendo todos".center(100))
