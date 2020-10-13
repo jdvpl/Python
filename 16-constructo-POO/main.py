01fc5437e037cebae2420246a6373883f6601206
Juan Daniel <juanda554242@gmail.com>
2020-10-12 19:26:36 -0500

    detectar tipado de un objeto

--- 16-constructo-POO/main.py
+++ 16-constructo-POO/main.py
@@ -2,5 +2,13 @@ from coche import Coche
 
 carro=Coche(input("Color: "),input("Marca: "),input("Modelo: "),int(input("Velocidad: ")),int(input("caballaje: ")),input("Plazas: "))
 carro1=Coche("Azul","vintage",2020,300,400,4)
+carro2=Coche("gris","mercedes",2020,500,400,4)
 print(carro.getInfo())
 print(carro1.getInfo())
+print(carro2.getInfo())
+
+# detectar tipado
+if type(carro2)==Coche:
+    print("Es un objeto correcto")
+else:
+    print("No es un objeto")
