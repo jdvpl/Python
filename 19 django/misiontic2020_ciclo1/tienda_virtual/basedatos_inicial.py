bfca3d7cceccb2ced21bb32dbae2e83ccf039317
Juan Daniel <juanda554242@gmail.com>
2020-10-16 18:36:31 -0500

    ingresando para mostrar los datos en el form

--- 19 django/misiontic2020_ciclo1/tienda_virtual/basedatos_inicial.py
+++ 19 django/misiontic2020_ciclo1/tienda_virtual/basedatos_inicial.py
@@ -2,3 +2,21 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db_tienda_virtual = client.tiendaVirtual
+
+lista_inicial = [
+    {'nombre': "Balón", 'costo': 10000},
+    {'nombre': "Chaqueta", 'costo': 85000},
+    {'nombre': "Libro", 'costo': 12000},
+    {'nombre': "Pantalón", 'costo': 90000},
+    {'nombre': "Computador", 'costo': 2500000},
+    {'nombre': "Silla", 'costo': 400000},
+    {'nombre': "Celular", 'costo': 1250000},
+    {'nombre': "Raqueta", 'costo': 250000},
+    {'nombre': "Tablet", 'costo': 600000},
+    {'nombre': "Mesa", 'costo': 172000},
+    {'nombre': "Camiseta", 'costo': 28000},
+    {'nombre': "Cámara", 'costo': 2450000},
+    {'nombre': "Billetera", 'costo': 62000},
+    {'nombre': "Sofá", 'costo': 450000},
+    {'nombre': "Alcancia", 'costo': 15000}
+]
